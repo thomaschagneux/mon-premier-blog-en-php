ab5bd59b903ac25c0277e6c6ed4cae100b6a24e7
Thomas Chagneux <thomas.chagneux@gmail.com>
2024-06-14 17:15:10 +0200

    P5-24 phpstan l6->7 complete

--- app/core/Router.php
+++ app/core/Router.php
@@ -12,7 +12,7 @@ namespace App\core;
 class Router
 {
     /**
-     * @var array<array{method: string, path: string, callback: callable|array{string, string}}> $routes Array of registered routes
+     * @var array<array{method: string, path: string, callback: callable|array{class-string, string}}> $routes Array of registered routes
      */
     private $routes = [];
 
@@ -21,11 +21,11 @@ class Router
      *
      * @param string $method HTTP method (e.g., 'GET', 'POST')
      * @param string $path The route path, with optional dynamic segments (e.g., '/user/{id}')
-     * @param callable|array{string, string} $callback The callback to be executed when the route is matched. This can be
+     * @param callable|array{class-string, string} $callback The callback to be executed when the route is matched. This can be
      *                                 a function or an array with a class and method (e.g., [HomeController::class, 'index'])
      * @return void
      */
-    public function addRoute($method, $path, $callback): void
+    public function addRoute(string $method, string $path, $callback): void
     {
         $path = preg_replace('/{[a-zA-Z0-9_]+}/', '([^/]+)', $path);
         $this->routes[] = [
@@ -44,37 +44,37 @@ class Router
      */
     public function handleRequest(HttpRequest $request): void
     {
-        // Iterate over each registered route
         foreach ($this->routes as $route) {
-            // Check if the HTTP method and URL match the current route
             if ($route['method'] === $request->getMethod() && preg_match($route['path'], $request->getUrl(), $matches)) {
-                // Remove the full match from the matches array, leaving only the captured groups
                 array_shift($matches);
-                
-                // Check if the callback is an array (i.e., a controller and method)
+
                 if (is_array($route['callback'])) {
-                    // Instantiate the controller
-                    $controller = new $route['callback'][0]();
-                    // Get the method name
-                    $method = $route['callback'][1];
-                    // Call the method on the controller with the captured URL parameters
-                    $response = call_user_func_array([$controller, $method], $matches);
+                    list($controllerName, $method) = $route['callback'];
+
+                    if (!class_exists($controllerName)) {
+                        throw new \RuntimeException("Controller class $controllerName does not exist");
+                    }
+
+                    $controller = new $controllerName();
+
+
+                    /** @var callable $callable */
+                    $callable = [$controller, $method];
+                    $response = call_user_func_array($callable, $matches);
                 } else {
-                    // Call the callback function with the captured URL parameters
+
                     $response = call_user_func_array($route['callback'], $matches);
                 }
-                // Check if the response is a string and output it
+
                 if (is_string($response)) {
                     echo $response;
                 }
-                // Stop processing further routes once a match is found
+
                 return;
             }
         }
 
-        // Default 404 handler if no route matches
         http_response_code(404);
         include __DIR__ . '/../Views/404.php';
-        
     }
 }
