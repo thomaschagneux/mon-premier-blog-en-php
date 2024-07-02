9b1450dbcc535727677d89f1d8104a20b5531e46
Thomas Chagneux <thomas.chagneux@gmail.com>
2024-07-02 11:50:54 +0200

    P5 resolve some errors on codacy

--- app/core/Database.php
+++ app/core/Database.php
@@ -49,10 +49,10 @@ class Database
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
         $dotenv->load();
 
-        $this->host = isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : '';
-        $this->db_name = isset($_ENV['DB_NAME']) ? $_ENV['DB_NAME'] : '';
-        $this->username = isset($_ENV['DB_USER']) ? $_ENV['DB_USER'] : '';
-        $this->password = isset($_ENV['DB_PASS']) ? $_ENV['DB_PASS'] : '';
+        $this->host = $this->getEnvVar('DB_HOST');
+        $this->db_name = $this->getEnvVar('DB_NAME');
+        $this->username = $this->getEnvVar('DB_USER');
+        $this->password = $this->getEnvVar('DB_PASS');
     }
 
     /**
@@ -91,5 +91,20 @@ class Database
     public function getDbName(): string
     {
         return $this->db_name;
+    }
+
+    /**
+     * Safely gets an environment variable and validates it.
+     *
+     * @param string $key The environment variable key
+     * @return string The sanitized environment variable value
+     */
+    private function getEnvVar(string $key): string
+    {
+        $value = $_ENV[$key] ?? false;
+        if ($value === false) {
+            throw new \Exception("Environment variable '$key' not found.");
+        }
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
     }
 }
