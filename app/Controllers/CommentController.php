869bc905468e2a177bb631e352b93ad4b7b13119
Thomas Chagneux <thomas.chagneux@gmail.com>
2024-10-08 11:36:07 +0200

    P5-42 create show comment

--- app/Controllers/CommentController.php
+++ app/Controllers/CommentController.php
@@ -3,18 +3,30 @@
 namespace App\Controllers;
 
 use App\core\Router;
+use App\Models\Comment;
+
 class CommentController extends AbstractController
 {
+    private Comment $commentModel;
+
     public function __construct(
         Router $router,
     )
     {
         parent::__construct($router);
+        $this->commentModel = new Comment();
     }
 
-    public function commentShow(int $commentId): void
+    public function commentShow(int $commentId)
     {
-
+        if ($this->isAdmin()) {
+           $comment = $this->commentModel->findById($commentId);
+           return $this->render('post/comment/show.html.twig', [
+               'comment' => $comment
+           ]);
+        } else {
+            return $this->redirectToReferer();
+        }
     }
 
     public function commentAddForm(int $postId): void
