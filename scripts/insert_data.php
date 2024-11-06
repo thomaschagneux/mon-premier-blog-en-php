8f3d46f71802d3f2f922d380863e8288a92ead68
Thomas Chagneux <thomas.chagneux@gmail.com>
2024-11-06 16:57:29 +0100

    P5-58 add picture on Post model

--- scripts/insert_data.php
+++ scripts/insert_data.php
@@ -37,7 +37,7 @@ function loadPictures($pdo) {
 function loadPosts($pdo) {
     $csvFile = dirname(__DIR__) . '/data/Post_Data.csv';
     insertDataFromCsv($pdo, $csvFile, 'post', [
-        'id', 'title', 'lede', 'content', 'user_id', 'created_at', 'updated_at'
+        'id', 'title', 'lede', 'featured_image_id', 'content', 'user_id', 'created_at', 'updated_at'
     ]);
 }
 
